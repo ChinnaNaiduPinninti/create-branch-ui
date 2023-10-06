4a2bfb2eb25e08bf932d97276cb6ccc1e611f95c
ChinnaNaiduPinninti <chinnanaidu@vkraftsoftware.com>
2023-10-06 14:00:01 +0530

    my code

---
+++ aia-server-5255/push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# Input string
+input_string="$1"
+cd C:/Users/vkraft/Desktop/devOps/git/create-branch-ui/aia-server-5255
+
+# Use 'cut' to split the string by ',' and assign to variables
+branchName=$(echo "$input_string" | cut -d ',' -f 1)
+
+message=$(echo "$input_string" | cut -d ',' -f 2)
+git switch $branchName
+
+git add --all
+
+git status
+
+git commit -m "$message"
+
+git push 
