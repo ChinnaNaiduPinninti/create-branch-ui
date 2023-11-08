1918e716b2f0cdc36680f786f75f920c691041fb
ChinnaNaiduPinninti <138875538+ChinnaNaiduPinninti@users.noreply.github.com>
2023-11-08 18:09:13 +0530

    Update clone

--- deleteClonedRepo.js
+++ deleteClonedRepo.js
@@ -1,14 +1,17 @@
+const fs = require('fs').promises;
 const express = require('express');
-const cors = require('cors');
-const { exec } = require('child_process');
 const bodyParser = require('body-parser');
-
 const app = express();
-const port = 3006;
+const port = 3007;
+const regex = /^\S+$/; // regex for null or empty or white space
+const cors = require('cors');
+const path = require('path');
+const git = require('simple-git');
+const exec = require('sync-exec');
 app.use(cors());
 
 const corsOptions = {
-  origin: 'http://myazedlap0010.aiaazure.biz:3006', // Replace with your frontend's URL
+  origin: 'http://myazedlap0010.aiaazure.biz:3000', // Replace with your frontend's URL
   methods: 'POST',
   allowedHeaders: 'Content-Type',
 };
@@ -20,51 +23,119 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-const fs = require('fs').promises; // Use promises-based filesystem module
+app.post('/createBranch', (req, res) => {
+  const branchName = req.body.projectName;
+  const branchCategory = req.body.branchCategory;
+  const branchType = req.body.branchType;
+  const serverName = req.body.serverName;
 
-app.post('/deleteClonedRepo', async (req, res) => {
-  const requestBody = req.body;
-  console.log(requestBody);
-  const projectName = req.body.projectName;
+  console.log(`branchType: ${branchType}`);
+  console.log(`branchCategory: ${branchCategory}`);
+  console.log(`serverName: ${serverName}`);
+  console.log(`branchName: ${branchName}`);
 
-  const filePath = `/opt/wm/node/clone/projectNames/${projectName}.txt`;
+  const gitDirectory = `${serverName}_${branchName}`;
+  console.log(`gitDirectory: ${gitDirectory}`);
 
-  try {
-    // Read the file
-    const data = await fs.readFile(filePath, 'utf8');
+  if (regex.test(branchName)) {
+    if (branchCategory === 'develop' || branchCategory === 'master') {
+      if (
+        branchType === 'bugfix/' ||
+        branchType === 'hotfix/' ||
+        branchType === 'release/' ||
+        branchType === 'other' ||
+        branchType === 'feature/'
+      ) {
+        const gitDirectoryPath = path.join('/opt', 'wm', 'git', gitDirectory, serverName);
 
-    // Now, 'data' contains the contents of the file.
-    // You can perform your operations on it here.
-    console.log('File data:', data);
-
-    // Example: Split the file contents by lines
-    const lines = data.split('\n');
-    console.log('File lines:', lines);
-
-    // Example: Process each line
-    lines.forEach(async (line, index) => {
-      console.log(`Line ${index + 1}: ${line.trim()}`);
-      if (line.trim() !== '') {
-        // Copy the server name from the line
-        serverName = line.trim();
-        const folderPath = `/opt/wm/git/${serverName}`;
-        console.log('Server Name:', serverName);
-
-        // Use try-catch to handle folder deletion
         try {
-          await fs.rm(folderPath, { recursive: true });
-          console.log(`Folder ${folderPath} deleted successfully.`);
-        } catch (error) {
-          console.error(`Error deleting folder: ${error.message}`);
-        }
-      }
-    });
+          // Change to the Git directory
+          process.chdir(gitDirectoryPath);
 
-    res.json({ message: 'Folders deleted successfully' });
-  } catch (error) {
-    console.error(`Error reading the file: ${error.message}`);
-    res.status(500).json({ error: 'Failed to read the file' });
+          // Switch to the specified branch category
+          exec(`git switch ${branchCategory}`, { stdio: 'inherit' });
+
+          if (branchType === 'other') {
+            // Create and switch to a new branch
+            exec(`git branch ${branchName}`, { stdio: 'inherit' });
+            exec(`git switch ${branchName}`, { stdio: 'inherit' });
+          } else {
+            // Create and switch to a new branch with branchType prefix
+            const newBranchName = `${branchType}${branchName}`;
+            exec(`git branch ${newBranchName}`, { stdio: 'inherit' });
+            exec(`git switch ${newBranchName}`, { stdio: 'inherit' });
+          }
+
+          res.status(201).send('Branch Created Successfully');
+        } catch (error) {
+          console.error('Error:', error.message);
+          res.status(400).send('Branch Not Created');
+        }
+      } else {
+        res.status(400).send('Invalid branchType');
+      }
+    } else {
+      res.status(400).send('Invalid branchCategory');
+    }
+  } else {
+    res.status(400).send('Invalid branchName');
   }
+});
+
+app.post('/executeGitClone', (req, res) => {
+  const serverName = req.body.serverName;
+  const branchName = req.body.projectName;
+  const repoUrl = `https://vishnusangaraju@bitbucket.org/myaia/${serverName}.git`;
+  const gitDirectory = `${serverName}_${branchName}/${serverName}`;
+  const targetDirectory = path.join('/opt/wm/git', gitDirectory);
+
+  console.log(`serverName: ${serverName}`);
+  console.log(`branchName: ${branchName}`);
+  console.log(`repoUrl: ${repoUrl}`);
+  console.log(`targetDirectory: ${targetDirectory}`);
+
+  async function createDirectoryIfNotExists(directoryPath) {
+    try {
+      await fs.mkdir(directoryPath, { recursive: true });
+      console.log('Directory created successfully:', directoryPath);
+    } catch (error) {
+      if (error.code !== 'EEXIST') {
+        console.error('Error creating directory:', error);
+        res.status(400).send('Branch Not Created');
+      }
+    }
+  }
+
+  async function cloneRepository(repoUrl, targetDirectory) {
+    await createDirectoryIfNotExists(targetDirectory);
+
+    try {
+      await git()
+        .silent(true)
+        .clone(repoUrl, targetDirectory);
+      console.log('Repository cloned successfully.');
+      res.status(201).send('Branch Created Successfully');
+    } catch (error) {
+      console.error('Error cloning repository:', error);
+      res.status(400).send('Branch Not Created');
+    }
+  }
+
+  const dir = '/opt/wm/node/projectNames/';
+  const filename = `${branchName}.txt`;
+  const data = `${serverName}`;
+
+  async function saveToFile() {
+    try {
+      await fs.writeFile(path.join(dir, filename), data);
+      console.log('Data has been written to the file in the specified directory');
+    } catch (err) {
+      console.error('Error writing to the file:', err);
+    }
+  }
+
+  saveToFile();
+  cloneRepository(repoUrl, targetDirectory);
 });
 
 app.listen(port, () => {
